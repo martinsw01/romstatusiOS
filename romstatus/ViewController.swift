a444879c2b78f2ac0dc0594f32068cff2b45ac75
martinsw01 <martin.s.winther@gmail.com>
2019-09-14 11:50:06 +0200

    New iOS app

---
+++ romstatus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  romstatus
+//
+//  Created by Martin Saue Winther on 14/09/2019.
+//  Copyright © 2019 akademiet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
